3fafb59f7bf36ad78edc10a23f858a9a9ba67998
Joan Sebastian Morales Pérez <josmoralesp0307@outlook.es>
2022-02-25 12:58:01 -0500

    Creando la conexión con la base de datos

---
+++ db/connection.php
@@ -0,0 +1,35 @@
+<?php
+
+class Conexion{
+    private $host = 'localhost';
+    private $user = 'root';
+    private $pass = '';
+    private $db = 'ecommerce_app';
+    private $mysqli;
+    private $resultado;
+
+
+    public function conectar(){
+        $this->mysqli = new mysqli($this->host, $this->user, $this->pass, $this->db);
+        $this->mysqli -> set_charset("utf8");
+    }
+
+    public function cerrarConexion(){
+        $this -> mysqli -> close();
+    }
+
+    public function ejecutar($sentencia){
+        $this -> resultado = $this -> mysqli -> query($sentencia);
+    }
+
+    public function extraer(){
+        return $this -> resultado -> fetch_row();
+    }
+
+    public function numFilas(){
+        return ($this -> resultado != null) ? $this -> resultado -> num_rows : 0;
+    }
+
+}
+
+?>
