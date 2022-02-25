fd381bbb24a2899ad3eb47fbdd3822fe0a6530fb
Joan Sebastian Morales Pérez <josmoralesp0307@outlook.es>
2022-02-25 16:36:15 -0500

    Cors para recibir peticiones

--- routes/user.routes.php
+++ routes/user.routes.php
@@ -1,8 +1,18 @@
 <?php
 
 require_once "C:/xampp/htdocs/ecommerce-core/controllers/user.controller.php";
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+
     $username = $_REQUEST["username"];
     $email = $_REQUEST["email"];
     $password = $_REQUEST["password"];
