f8f456a79955f7c2037970c17dde15d1d8e80041
Paula Sierra <PaulaCode01@gmail.com>
2022-03-03 21:26:33 -0500

    Controlador de categoría

---
+++ models/CategoryDAO.php
@@ -0,0 +1,34 @@
+<?php
+
+class CategoryDAO
+{
+    private $id;
+    private $category;
+
+    public function __construct($id, $category)
+    {
+        $this->id = $id;
+        $this->category = $category;
+    }
+
+    public function getAll(){
+        return "SELECT * FROM category";
+    }
+
+    public function getById($id){
+        return "SELECT * FROM category WHERE id = $id";
+    }
+    public function crear(){
+        return "insert into category(category)
+            values('" . $this -> category . ")";
+    }
+
+    public function actualizar(){
+        return "update category set
+                category = '" . $this -> category . "',
+                where id = '" . $this -> id . "'";
+    }
+    public function eliminar(){
+        return "delete from category where id ". $this -> id;
+    }
+}
