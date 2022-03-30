16c60cc1a2f8dd76375ddb6eaf4e3b311937acdc
Joan Morales <josmorales0307@outlook.es>
2022-03-30 13:38:04 -0500

    ajustes de venta

--- controllers/Sale.controller.php
+++ controllers/Sale.controller.php
@@ -7,21 +7,23 @@ class SaleController
 {
     private $id;
     private $date;
+    private $id_seller;
     private $id_client;
     private $state;
     private $total;
     private $conexion;
     private $saleDAO;
 
-    public function __construct($id = 0, $date = "", $id_client = 0, $state = 0, $total = 0)
+    public function __construct($id = 0, $date = "", $id_seller = 0, $id_client = 0, $state = 0, $total = 0)
     {
         $this->id = $id;
         $this->date = $date;
+        $this->id_seller = $id_seller;
         $this->id_client = $id_client;
         $this->state = $state;
         $this->total = $total;
         $this->conexion = new Conexion();
-        $this->saleDAO = new SaleDAO($this->id, $this->date, $this->id_client, $this->state, $this->total);
+        $this->saleDAO = new SaleDAO($this->id, $this->date, $this->id_seller, $this->id_client, $this->state, $this->total);
     }
 
 
